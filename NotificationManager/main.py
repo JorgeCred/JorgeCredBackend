bb2959fc4f7427c250b3464d487d8af77d1b3082
CaioPinho9 <caiopinho9@gmail.com>
2023-10-28 14:36:09 -0300

    feat: .env

--- NotificationManager/main.py
+++ NotificationManager/main.py
@@ -1,11 +1,22 @@
+from os import environ
+
 import pika
 import sys
 
-credentials = pika.PlainCredentials('fzrrkjya', '4hKfmc3shjyOXU4Jr_ptSem0hh5LfP-0')
+from dotenv import load_dotenv
+
+load_dotenv()
+
+RABBITMQ_USERNAME = environ['RABBITMQ_USERNAME']
+RABBITMQ_PASSWORD = environ['RABBITMQ_PASSWORD']
+RABBITMQ_HOST = environ['RABBITMQ_HOST']
+RABBITMQ_PORT = environ['RABBITMQ_PORT']
+
+credentials = pika.PlainCredentials(RABBITMQ_USERNAME, RABBITMQ_PASSWORD)
 parameters = pika.ConnectionParameters(
-    'chimpanzee.rmq.cloudamqp.com',
-    5672,
-    'fzrrkjya',
+    RABBITMQ_HOST,
+    RABBITMQ_PORT,
+    RABBITMQ_USERNAME,
     credentials,
 )
 
@@ -19,5 +30,3 @@ message = ' '.join(sys.argv[1:]) or "info: Hello World!"
 channel.basic_publish(exchange='Bitu', routing_key='', body=message)
 print(f" [x] Sent {message}")
 connection.close()
-
-
