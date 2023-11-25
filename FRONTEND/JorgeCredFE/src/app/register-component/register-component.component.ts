d09783e07cb6d9eda0854ba1bdb1d819d1c52e5d
Gustavo da Rocha Pereira <gustavo.rocha@harbor.com.br>
2023-11-25 19:19:00 -0300

    orge

---
+++ FRONTEND/JorgeCredFE/src/app/register-component/register-component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-register-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './register-component.component.html',
+  styleUrl: './register-component.component.scss'
+})
+export class RegisterComponentComponent {
+
+}
